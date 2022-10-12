521575a67cc3f88db98dfd4c3b740fbc9ae55533
b7c <58299468+b7c@users.noreply.github.com>
2022-10-13 05:49:47 +1300

    Fix DateTimeConverter

--- Xabbo.Web/Serialization/DateTimeConverter.cs
+++ Xabbo.Web/Serialization/DateTimeConverter.cs
@@ -2,21 +2,16 @@
 
 namespace Xabbo.Web.Serialization;
 
-public class DateTimeConverter : JsonConverter<DateTime?>
+public class DateTimeConverter : JsonConverter<DateTime>
 {
-    public override bool CanConvert(Type typeToConvert)
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        return typeof(DateTime?).IsAssignableFrom(typeToConvert);
+        string dateString = reader.GetString() ?? throw new FormatException("Date/time cannot be null.");
+        return DateTime.Parse(dateString);
     }
 
-    public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
     {
-        string? dateString = reader.GetString();
-        return dateString is null ? null : DateTime.Parse(dateString);
-    }
-
-    public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
-    {
-        writer.WriteStringValue(value?.ToString("yyyy-MM-dd'T'HH:mm:ss.fff+0000"));
+        writer.WriteStringValue(value.ToString("yyyy-MM-dd'T'HH:mm:ss.fff+0000"));
     }
 }
