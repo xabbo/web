2c70d5655990af684da172df5f4770a8941e898b
b7c <58299468+b7c@users.noreply.github.com>
2022-10-13 16:45:47 +1300

    Add PhotoInfo

---
+++ Xabbo.Web/Photo/PhotoInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Xabbo.Web;
+
+public class PhotoInfo
+{
+    [JsonPropertyName("t")]
+    public long Time { get; set; }
+
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+}
